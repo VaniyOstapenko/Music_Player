f5d5618a965ba18308094765b105424119e93c54
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2023-10-12 18:15:03 +0300

    added music player

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const img = document.querySelector('.img');
 const nameOfSong = document.querySelector('.nameOfSong');
 const artist = document.querySelector('.artist');
 const slider = document.querySelector('.slider');
+const progress = document.querySelector('.progress');
 
 const repeat = document.querySelector('.repeat');
 const back = document.querySelector('.back');
@@ -29,7 +30,6 @@ play.addEventListener('click', () => {
     artist.textContent = music[IndexSong].artist;
     img.style = music[IndexSong].img;
     nameOfSong.textContent = music[IndexSong].nameOfSong;
-
     if (flag === false) {
         audio.play();
         play.style = 'background-image: url(./assets/pause.svg)';
@@ -65,3 +65,46 @@ next.addEventListener('click', () => {
     play.style = 'background-image:url(./assets/pause.svg)';
 })
 
+heart.addEventListener('click', () => {
+    if (flag === false) {
+        heart.style = 'background-image: url(./assets/blackHeart.png)';
+        flag = true;
+    } else {
+        heart.style = 'background-image: url(./assets/heart.png)';
+        flag = false;
+    }
+})
+
+audio.addEventListener('timeupdate', (e) => {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`
+});
+
+slider.addEventListener('click', (e) => {
+    const width = slider.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+});
+
+audio.addEventListener('ended', () => {
+    IndexSong++
+    if (IndexSong > music.length - 1) IndexSong = 0;
+    audio.src = (music[IndexSong].path);
+    artist.textContent = music[IndexSong].artist;
+    img.style = music[IndexSong].img;
+    nameOfSong.textContent = music[IndexSong].nameOfSong;
+    audio.play();
+})
+
+audio.addEventListener('timeupdate', ({ target }) => {
+    const { currentTime } = target;
+    const min = Math.floor(currentTime / 60);
+    const sec = Math.floor(currentTime % 60)
+    if (sec < 10) {
+        time.innerHTML = `0${min}:0${sec}`;
+    } else {
+        time.innerHTML = `0${min}:${sec}`;
+    }
+});
